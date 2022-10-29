d6d5a8baf459bb07bfefa2173933510a6747a47e
demidraksin <greatraksin@icloud.com>
2022-10-29 18:27:32 +0300

    Adding arrays and loops

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 
 int main() {
-
+    // abs - принимает число и возвращает модуль этого числа
+    cout << abs(-5) << endl;
+    cout << abs(-3728) << endl;
+    cout << abs(56) << endl;
+    cout << abs(3 - 9) << endl;
 }
