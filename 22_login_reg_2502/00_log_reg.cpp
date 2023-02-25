b92e1a96d65e2ba231c6f46267563ca0b0bcf3fd
100ballovby <legorobotsbel@gmail.com>
2023-02-25 18:01:36 +0300

    Adding caesar cipher

---
+++ 22_login_reg_2502/00_log_reg.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int menu() {
+    cout << "Please, choose option: \n";
+    cout << "0 - exit\n1 - Register\n2 - Login\n\n";
+    int ans;
+    cin >> ans;
+    return ans;
+}
+
+bool isLoggedIn(string username, string password) {  // функция, которая будет проверять, вошел ли пользователь в систему
+    string uname, passw;  // сюда будем сохранять данные пользователя из файла
+    ifstream read("../data/" + username + ".txt");  // открываю файл с названием username.txt из папки data
+    getline(read, uname);  // считываю username и присваиваю его переменной uname
+    getline(read, passw);  // считываю password и присваиваю его переменной passw
+
+    read.close();  // закрываем файл после работы
+
+    if (uname == username && passw == password) {  // если данные от пользователя совпадают с данными из файла
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool userRegister(string username, string password, string confirmation) {
+    if (password == confirmation) {
+        ofstream file;
+        file.open("../data/" + username + ".txt", ios::out);
+        file << username << endl << password;
+        file.close();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// переберем названия файлов в папке data
+
+
+int zapusk() {
+    int option = 3;
+    while (option != 0) {
+        option = menu();
+
+        if (option == 1) {
+            string login, password, confirm;
+            cout << "Login: "; cin >> login;
+            cout << "Password: "; cin >> password;
+            cout << "Password (again): "; cin >> confirm;
+
+            bool status = userRegister(login, password, confirm);
+
+            if (status) {  // if (status == true)
+                cout << "Successfully registration!\n\n";
+            } else {
+                cout << "Passwords aren't math, try again!\n\n";
+            }
+        } else if (option == 2) {
+            string login, password;
+            cout << "Your login (username): "; cin >> login;
+            cout << "Your password: "; cin >> password;
+            bool status = isLoggedIn(login, password);
+
+            if (status) {  // if (status == true)
+                cout << "Successfully logged in!\n\n";
+            } else {
+                cout << "Login or/and password is incorrect!\n\n";
+            }
+        }
+    }
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,77 +1,52 @@
 #include <iostream>
-#include <fstream>
 #include <string>
 using namespace std;
 
-int menu() {
-    cout << "Please, choose option: \n";
-    cout << "0 - exit\n1 - Register\n2 - Login\n\n";
-    int ans;
-    cin >> ans;
-    return ans;
-}
+string caesar_cipher(string phrase, int shift, string lang) {
+    string eng = "abcdefghijklmnopqrstuvwxyz";
+    string rus = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    string alphabet = "";
+    string res = "";  // зашифрованную строчку буду хранить здесь
 
-bool isLoggedIn(string username, string password) {  // функция, которая будет проверять, вошел ли пользователь в систему
-    string uname, passw;  // сюда будем сохранять данные пользователя из файла
-    ifstream read("../data/" + username + ".txt");  // открываю файл с названием username.txt из папки data
-    getline(read, uname);  // считываю username и присваиваю его переменной uname
-    getline(read, passw);  // считываю password и присваиваю его переменной passw
-
-    read.close();  // закрываем файл после работы
-
-    if (uname == username && passw == password) {  // если данные от пользователя совпадают с данными из файла
-        return true;
+    if (tolower(lang[0]) == 'r') {
+        alphabet = rus;
     } else {
-        return false;
+        alphabet = eng;
     }
-}
 
-bool userRegister(string username, string password, string confirmation) {
-    if (password == confirmation) {
-        ofstream file;
-        file.open("../data/" + username + ".txt", ios::out);
-        file << username << endl << password;
-        file.close();
-        return true;
-    } else {
-        return false;
-    }
-}
+    for (int i = 0; i < phrase.length(); i++) {
+        int place = alphabet.find(phrase[i]);  // определяем индекс буквы в алфавите
+        int new_place = place + shift;  // сохраняем новую позицию буквы в алфавите
 
-// переберем названия файлов в папке data
+        if (new_place >= alphabet.length()) {
+            new_place -= alphabet.length();
+        }
 
-
-int main() {
-    int option = 3;
-    while (option != 0) {
-        option = menu();
-
-        if (option == 1) {
-            string login, password, confirm;
-            cout << "Login: "; cin >> login;
-            cout << "Password: "; cin >> password;
-            cout << "Password (again): "; cin >> confirm;
-
-            bool status = userRegister(login, password, confirm);
-
-            if (status) {  // if (status == true)
-                cout << "Successfully registration!\n\n";
-            } else {
-                cout << "Passwords aren't math, try again!\n\n";
-            }
-        } else if (option == 2) {
-            string login, password;
-            cout << "Your login (username): "; cin >> login;
-            cout << "Your password: "; cin >> password;
-            bool status = isLoggedIn(login, password);
-
-            if (status) {  // if (status == true)
-                cout << "Successfully logged in!\n\n";
-            } else {
-                cout << "Login or/and password is incorrect!\n\n";
-            }
+        if (ispunct(phrase[i]) || isspace(phrase[i])) {
+            res += phrase[i];  // не меняю символ, если он является знаком препинания
+        } else {
+            res += alphabet[new_place];  // подставляю букву со сдвигом в строку
         }
     }
+    return res;
+}
 
+string decipher(string phrase, string lang) {
+    string eng = "abcdefghijklmnopqrstuvwxyz";
+    string rus = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    string alphabet = "";
+    string res = "";  // зашифрованную строчку буду хранить здесь
+
+    if (tolower(lang[0]) == 'r') {
+        alphabet = rus;
+    } else {
+        alphabet = eng;
+    }
+
+    return res;
+}
+
+int main() {
+    cout << caesar_cipher("мама мыла раму, где мама?", 4, "rus") << endl;
     return 0;
 }
