6782b6982c1e2364174a4377a1073d3b06324e51
greatraksin <greatraksin@icloud.com>
2023-03-04 18:17:44 +0300

    Adding vector

--- 24_vector_0403/00_vector.cpp
+++ 24_vector_0403/00_vector.cpp
@@ -2,6 +2,13 @@
 #include <vector>
 using namespace std;
 
+void print_vector(vector<int> v) {
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << ' ';
+    }
+    cout << endl;
+}
+
 void vector_basics() {
     // Нет необходимости указывать длину при иницилизации
     vector<int> v_array;
@@ -47,4 +54,96 @@ void vector_initializing() {
     for (auto &element : v_array3) {
         cout << element << ' ';
     }
+}
+
+void vector_push_back() {
+    // создаем вектор строк
+    vector<string> students;
+    // временное хранилище фамилий студентов
+    string buffer;
+
+    cout << "Insert students names. Insert empty string for exit: " << endl;
+    do {
+        getline(cin, buffer);  // получаю ввод от пользователя
+        if (!buffer.empty()) {  // если нам ввели не пустую строку
+            students.push_back(buffer);  // добавляем занчение в вектор
+            // метод push_back(x) добавляет значение х в конец вектора
+        }
+    } while (buffer != "");  // проверяю: не пустой ли он
+
+    unsigned int vector_size = students.size();// метод push_back(x) добавляет значение х в конец вектора
+
+    // выведем заполненный вектор в консоль
+    cout << "Students: " << endl;
+    for (auto &student : students) {
+        cout << student << endl;
+    }
+}
+
+void vector_add_elements() {
+    vector<int> numbers = {10, 8, 6, 4, 2, 1};
+
+    numbers.emplace_back(8);  // этот метод тоже добавляет элемент в конец вектора
+    print_vector(numbers);
+
+    auto start = numbers.begin();
+    // insert(position, value) - вставляет value на место position
+    numbers.insert(start + 2, 17);
+    /** begin() определяет место в памяти, где начинается вектор. Фиксирует позицию первого элемента,
+     * мы добавляем к начальной позиции 2, чтобы указать, что работа с вектором начинается со второго
+     * индекса (индекс первого (0) + 2 = 2)**/
+    print_vector(numbers);
+
+    // если передать insert 3 аргумента, вы сможете вставить опредленное количество элементов на место другого элемента
+    numbers.insert(start + 3, 5, 10);  // на место элемента с индексом 3 вставить 5 десяток
+    print_vector(numbers);
+}
+
+void vector_delete_element() {
+    vector<int> numbers = {10, 8, 6, 4, 2, 1};
+    // Удаление элементов вектора
+    /**
+     * - erase(ptr) - удаляет из вектора элемент, на который указывает ptr. После удаления элемента
+     * ptr начинает указывать на элемент, который стоит следующим после удаленного или, если такого
+     * элемента нет, то указывает на конец вектора.
+     * **/
+
+    auto start = numbers.begin();
+    auto end = numbers.end();  // фиксирую точку в конце вектора
+    numbers.erase(end - 1, end);  // удалить элемент с -1 (предпоследнего) до последнего
+    print_vector(numbers);
+
+
+    start = numbers.begin();
+    end = numbers.end();  // фиксирую точку в конце вектора
+    // хочу удалить с 3 индекса до 7
+    numbers.erase(start + 2, end);  // удалить элемент с 3 до -1
+    print_vector(numbers);
+}
+
+void vector_comparison() {
+    vector<int> v1 {1, 2, 3};
+    vector<int> v2 {1, 2, 3};
+    vector<int> v3 {3, 2, 1};
+
+    // вектора можно сравнивать между собой
+    bool v1_equals_v2 = v1 == v2;  // true
+    bool v1_equals_v3 = v1 != v3;  // true
+    bool v2_equals_v3 = v2 == v3;  // false
+
+    cout << "Vector v1 equals v2: " << v1_equals_v2 << endl;
+    cout << "Vector v1 not equals v3: " << v1_equals_v3 << endl;
+    cout << "Vector v2 equals v3: " << v2_equals_v3 << endl;
+    /** при сравнении двух векторов операторами сравнения
+     * происходит сравнение всех элементов вектора и, если
+     * вектора содержат одни и те же элементы, вектора будут равны **/
+
+    bool vector_are_equal = true;
+    for (int i = 0; i < v1.size(); i++) {  // в подобном сравнении нет смысла, потому что вектора можно сравнивать обычными операторами
+        if (v1[i] != v2[i]) {
+            vector_are_equal = false;
+            break;
+        }
+    }
+    cout << "Vector v1 are equal v2: " << vector_are_equal << endl;
 }
--- main.cpp
+++ main.cpp
@@ -1,29 +1,6 @@
 #include <iostream>
-#include <vector>
-#include <string>
 using namespace std;
 
 int main() {
-    // создаем вектор строк
-    vector<string> students;
-    // временное хранилище фамилий студентов
-    string buffer;
-
-    cout << "Insert students names. Insert empty string for exit: " << endl;
-    do {
-        getline(cin, buffer);  // получаю ввод от пользователя
-        if (!buffer.empty()) {  // если нам ввели не пустую строку
-            students.push_back(buffer);  // добавляем занчение в вектор
-            // метод push_back(x) добавляет значение х в конец вектора
-        }
-    } while (buffer != "");  // проверяю: не пустой ли он
-
-    unsigned int vector_size = students.size();// метод push_back(x) добавляет значение х в конец вектора
-
-    // выведем заполненный вектор в консоль
-    cout << "Students: " << endl;
-    for (auto &student : students) {
-        cout << student << endl;
-    }
     return 0;
 }
